6a2c88ed2a7c3b2cb02a9c8ed8357dce070adc85
pr0methevs <artxk92@gmail.com>
2025-06-10 20:29:35 -0400

    Initial commit

---
+++ src/main/java/us/homelab_core/ui/example/front_end_app/FrontEndAppApplication.java
@@ -0,0 +1,13 @@
+package us.homelab_core.ui.example.front_end_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/homelab_core/ui/example/front_end_app/FrontEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package us.homelab_core.ui.example.front_end_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
