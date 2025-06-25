3033d7466dee523da3cfc02f95e70174256e0cd8
prometheus <devartk.dev@gmail.com>
2025-06-25 01:20:38 +0000

    refactor: rename from InfoComponent to AboutComponent

--- src/main/webapp/src/app/about/about.component.spec.ts
+++ src/main/webapp/src/app/about/about.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InfoComponent } from './about.component';
+import { AboutComponent } from './about.component';
 
 describe('InfoComponent', () => {
-  let component: InfoComponent;
-  let fixture: ComponentFixture<InfoComponent>;
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InfoComponent]
+      imports: [AboutComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(InfoComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
